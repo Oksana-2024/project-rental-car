e719bf0976b8a9661dae7bb7978c1b3a763a016a
Oksana <oksanavasilevna77@gmail.com>
2025-05-20 23:06:25 +0300

    add loader for button LoadMore

--- src/pages/CarsPage.tsx
+++ src/pages/CarsPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 import { useAppDispatch } from "../hooks/useAppDispatch";
@@ -11,15 +11,22 @@ import CarList from "../components/CarList/CarList";
 import Container from "../components/Container/Container";
 import SearchBar from "../components/SearchBar/SearchBar";
 import s from "./CarsPage.module.css";
+import { ClipLoader } from "react-spinners";
 
 const CarsPage = () => {
   const dispatch = useAppDispatch();
   useEffect(() => {
     dispatch(getBrands());
   }, [dispatch]);
-
+  const [isLoading, setIsLoading] = useState(false);
   const page = useSelector(selectPage);
   const totalPages = useSelector(selectTotalPages);
+  const handleClick = () => {
+    setIsLoading(true);
+    dispatch(loadMoreCars())
+      .unwrap()
+      .finally(() => setIsLoading(false));
+  };
 
   return (
     <section className={s.carsPage}>
@@ -27,14 +34,22 @@ const CarsPage = () => {
         <SearchBar />
 
         <CarList />
-        {totalPages > page && (
-          <Button
-            onClick={() => dispatch(loadMoreCars())}
-            text="Load more"
-            type="button"
-            className={s.loadMoreBtn}
-          />
-        )}
+        {totalPages > page &&
+          (!isLoading ? (
+            <Button
+              onClick={handleClick}
+              text="Load more"
+              type="button"
+              className={s.loadMoreBtn}
+            />
+          ) : (
+            <div className={s.smallLoader}>
+              <ClipLoader
+                size={80}
+                color="var(--accent-color)"
+              />
+            </div>
+          ))}
       </Container>
     </section>
   );
